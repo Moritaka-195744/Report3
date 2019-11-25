7eb4c4925a8e9f65c809f2247b6a9d5093fb2918
e195744 <e195744@ie.u-ryukyu.ac.jp>
2019-11-26 01:26:23 +0900

    レポート３　コード

--- src/main/jp/ac/uryukyu/ie/e195744/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195744/Enemy.java
@@ -8,12 +8,7 @@ package jp.ac.uryukyu.ie.e195744;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
-
+public class Enemy extends LivingThing{
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
      * @param name モンスター名
@@ -26,31 +21,6 @@ public class Enemy {
         this.attack = attack;
         dead = false;
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead() {
-        return dead;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if(!isDead()) {
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
     }
 
     /**
